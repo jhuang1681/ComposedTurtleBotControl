c4e6540f63c5511294a6d2c09641d4a323a07824
Jiahui <huan1681@purdue.edu>
2026-04-06 21:48:05 +0000

    edited pid_controller

--- pid_controller.py
+++ pid_controller.py
@@ -1,19 +1,19 @@
 import argparse
-from dataclasses import asdict, dataclass, field
-from pathlib import Path
-import random
-import sys
-from typing import Any, Dict, List, Optional, Tuple
+# from dataclasses import asdict, dataclass, field
+# from pathlib import Path
+# import random
+# import sys
+# from typing import Any, Dict, List, Optional, Tuple
 
 import gymnasium as gym
 import numpy as np
 import pandas as pd
 import rclpy
 
-import tb4_drl_navigation.envs  # noqa: F401
-import torch
-import torch.nn as nn
-import time
+import tb4_drl_navigation.envs # noqa: F401
+# import torch
+# import torch.nn as nn
+# import time
 import yaml
 from transforms3d.euler import quat2euler, euler2quat
 from get_path import get_path
@@ -68,8 +68,8 @@ def get_robot_xy(env):
     return np.array([agent_x, agent_y]), yaw
 
 import rclpy
-from rclpy.node import Node
-from nav_msgs.msg import Odometry
+# from rclpy.node import Node
+# from nav_msgs.msg import Odometry
 
 
 def main():
@@ -90,9 +90,8 @@ def main():
 
     rclpy.init()
     print("rclpy init")
-
-    env=gym.make('Turtlebot4Env-v0')
-    print("env made")
+    
+    env = gym.make('Turtlebot4Env-v0')
 
     start_pos, goal_pos, path, is_loop = get_path(pid_config["path"], pid_config["slope"])
 
